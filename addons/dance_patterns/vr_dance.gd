b0ddcc399f19806347dda687a0012cb2b5450682
Tim McNamara <timothyjmcnamara@Gmail.com>
2023-03-29 22:18:38 +0100

    Update vr_dance.gd

--- addons/dance_patterns/vr_dance.gd
+++ addons/dance_patterns/vr_dance.gd
@@ -5,4 +5,5 @@ func _enter_tree():
 	add_custom_type("Dance Pattern", "Spatial", preload("dance_pattern.gd"), preload("mu_icon.png"))
 	
 func _exit_tree():
-	pass
+	remove_custom_type("Dance Pattern")
+	
